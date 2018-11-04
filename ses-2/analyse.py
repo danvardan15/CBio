132c813acdd85984b0cd7c440f4be5485836ec71
Daniel Vargas <daniel.vhya@gmail.com>
2018-11-04 19:04:54 +0100

    Add function to get the best structure from 25 lower energy states

--- ses-2/analyse.py
+++ ses-2/analyse.py
@@ -6,8 +6,8 @@ from PymolLauncher import *
 import pymol
 from pymol import cmd
 
-def read_scores(score_path):
-    data = np.genfromtxt(score_path, dtype=object)
+def read_scores(score_path, skip_header):
+    data = np.genfromtxt(score_path, dtype=object, skip_header=skip_header)
     mask = np.logical_or(data[0]==b'score', data[0]==b'gdtmm')
     mask[-1] = True
     mat = data[:, mask]
@@ -17,25 +17,39 @@ def read_scores(score_path):
     name = name.astype('str')
     return gdt_ts, energy, name
 
-def plot_read_scores(score_path):
-    x, y, annote = read_scores(score_path)
+def plot_scores(x, y):
     f, ax = plt.subplots()
     ax.scatter(x, y)
     ax.set_title("GDT_TS vs. Energy of protein") #specify
     ax.set_xlabel("GDT_TS [gdt_ts units]") # units?
     ax.set_ylabel("Energy [pJ / mol]") # units?
     ax.set_xlim(0.0, 1.0)
-    return x, y, annote, ax
+    return ax
 
 def load_native(pl1):
     cmd.load(pl1.native)
 
+def stats(x, y, annote):
+    # What is the structure with the highest GDT_TS
+    # among the 25 lowest energy structures?
+    # What is the median GDT_TS
+    lowest_25 = sorted([(z[1],z[2]) for z in sorted(zip(y, x, annote))][:25])
+    s_best = lowest_25[-1][1]
+    s_median = lowest_25[12][0]
+    return s_best, s_median
+
 @cmd.extend
-def analyse(protein='1N0U'):
+def analyse(protein='1N0U', skip_header=0):
     score_path = '{}/score.fsc'.format(protein)
-    x, y, annote, ax = plot_read_scores(score_path)
+    x, y, annote = read_scores(score_path, skip_header)
+    ax = plot_scores(x, y)
+    s_best, s_median = stats(x, y, annote)
+    print("among the 25 lowest energy structures:")
+    print("best structrure: {}".format(s_best))
+    print("Median gdt_ts: {}".format(s_median))
     pl1 = PymolLauncher(x, y, annote, ax)
     pl1.set_native("{0}/{0}.pdb".format(protein))
     load_native(pl1)
     plt.connect('button_press_event', pl1)
     plt.show()
+
