ee1fd31c24707a7fae7909885f283452cb21e2a6
danvardan15 <daniel.vhya@gmail.com>
2018-11-10 18:31:30 +0100

    Added energy plot

--- ses-3/metropolis.py
+++ ses-3/metropolis.py
@@ -1,21 +1,18 @@
 import numpy as np
 import pyrosetta
+import matplotlib.pyplot as plt
 from pyrosetta import PyMOLMover
+
 pyrosetta.init()
 
-pmover = PyMOLMover()
-score3 = pyrosetta.create_score_function("score3")
-
-# initialize
-pose = pyrosetta.pose_from_sequence('A'*10)
-for res in range(1, pose.total_residue() +1):
-    pose.set_phi(res, -150)
-    pose.set_psi(res, 150)
-    pose.set_omega(res, 180)
-
-pmover.apply(pose)
-
-print("Initial energy: {}".format(score3(pose)))
+def simple_pose():
+    # Create a dummy pose with certain initial angles
+    pose = pyrosetta.pose_from_sequence('A'*50)
+    for res in range(1, pose.total_residue() +1):
+        pose.set_phi(res, -150)
+        pose.set_psi(res, 150)
+        pose.set_omega(res, 180)
+    return pose
 
 def modular_angle(n):
     if np.abs(n) > 180: n = -np.sign(n) * 360 + n
@@ -37,8 +34,7 @@ def limited_move(pose, res, score_fn, max_add=25):
     # decide with energy difference + Met criterion
     dE = E2 - E1
     if dE < 0: return candidate, E2
-    if np.random.random() < metropolis_crit(dE, kT=1):
-        return candidate, E2
+    if np.random.random() < metropolis_crit(dE, kT=1): return candidate, E2
     return pose, E1
 
 
@@ -46,8 +42,9 @@ def metropolis(pose, score_fn, pmover, niter=10):
     # Get initial score
     c = 0
     E = score_fn(pose)
-    residue = np.random.randint(1, pose.total_residue() + 1)
+    Es = []
     for i in range(niter):
+        residue = np.random.randint(1, pose.total_residue() + 1)
         new_pose, new_E = limited_move(pose, residue, score_fn)
         if E != new_E:
             # If there is a change in energy, accept changes
@@ -55,8 +52,21 @@ def metropolis(pose, score_fn, pmover, niter=10):
             pmover.apply(pose)
             E = new_E
             c += 1
-    print(c)
-    return pose
+            Es.append(E)
+    return pose, Es
 
-pose = metropolis(pose, score3, pmover, 100)
+pmover = PyMOLMover()
+score3 = pyrosetta.create_score_function("score3")
+
+pose = simple_pose()
+print("Initial energy: {}".format(score3(pose)))
+pmover.apply(pose)
+pose, Es = metropolis(pose, score3, pmover, 1000)
+
 print("Final energy: {}".format(score3(pose)))
+plt.figure()
+plt.plot(Es)
+plt.xlabel("Iteration")
+plt.ylabel("Score 3")
+plt.show()
+
